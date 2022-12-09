1996e8b3353f8fe57f9c7640a9b41046a6630cc7
BrandonF55 <brandon.fenk99@gmail.com>
2022-12-09 13:35:01 -0800

    now have full UI but its still a WIP

--- PierresBakery/Models/Programes.cs
+++ PierresBakery/Models/Programes.cs
@@ -3,7 +3,7 @@ using PierresBakery;
 using System.Collections.Generic;
 
 
-namespace PierresBakery
+namespace PierresBakery.Program
 {
     public class Program
     {
@@ -15,17 +15,24 @@ namespace PierresBakery
             Console.WriteLine("We have Bread and Pastries available today!: ");
             Console.WriteLine("Bread: A single loaf cost $5. Buy 2, get 1 free!");
             Console.WriteLine("Pastries: Buy 1 for $2 or 3 for $5.");
-            Console.WriteLine("Would you like to place a order? [Y/y for YES; Press Enter for No]");
+            // Console.WriteLine("Would you like to place a order? [Y/y for YES; Press Enter for No]");
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            string userInputOrder = Console.WriteLine().ToUpper();
+
+          static void TryAgain()  
+          
+                {
+            Console.WriteLine("Would you like to place a order? [Y/y for YES; Press Enter for No]");
+            string userInputOrder = Console.WriteLine();//.ToUpper();
             if (userInputOrder == "Y")
             {
                 int cost = 0;
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 Console.WriteLine("How many loafs of bread would you like?:");
-                int loafsWanted = int.parse(Console.ReadLine());
+                int loafsWanted = int.Parse(Console.ReadLine());
                 Console.WriteLine("How many pastries would you like to purchase?:");
-                int pastriesWanted = int.pase(Console.ReadLine());
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                int pastriesWanted = int.Parse(Console.ReadLine());
 
                 Bread loafs = new Bread(loafsWanted);
                 Pastery item = new Pastery(pastriesWanted);
@@ -36,15 +43,32 @@ namespace PierresBakery
                         cost -= (loafs.Counter/3) * 5;
                     }
 
-                    if (item.Counter % == 0);
+                    if(item.Counter % 3 == 0)
                     {
-                        cost -= (cake.Counter/3);
+                        cost -= (item.Counter/3);
+                    }
+
+                    Console.WriteLine("Your order is: ");
+                    Console.WriteLine($" Bread: {loafsWanted}, Pastries {pastriesWanted}");
+                    Console.WriteLine($" Your Order Total is: {cost}");
+                    Console.WriteLine("Please enter your Payment: ");
+
+                    int paymentResponse = int.Parse(Console.ReadLine());
+                    
+                    if(paymentResponse == cost)
+                    {
+                        Console.WriteLine("Thank you for shopping with us!");
+                    }
+                    else 
+                    {
+                        Console.Write("My apologies, but that response was not valid please try again.");
+                        TryAgain();
                     }
 
 
 
                 
-
+                }
 
             }
 
