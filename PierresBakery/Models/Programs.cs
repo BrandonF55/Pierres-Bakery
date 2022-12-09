027e976d9eadb4a52d660e78190a78dc1f453a76
BrandonF55 <brandon.fenk99@gmail.com>
2022-12-09 14:31:26 -0800

    now have updated README.md file

--- PierresBakery/Models/Programs.cs
+++ PierresBakery/Models/Programs.cs
@@ -12,26 +12,26 @@ namespace PierresBakery.Program
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine("Welcome To Pierre's Baked Goods!");
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("We have Bread and Pastries available today!:");
-            Console.WriteLine("Bread: A single loaf cost $5! Buy 2, get 1 free!");
-            Console.WriteLine("Pastries: Buy 1 for $2 or 3 for $5.");
+            Console.WriteLine("We Have Bread And Pastries Available Today!:");
+            Console.WriteLine("Bread: A Single Loaf Cost $5! Buy Two, Get One Free!");
+            Console.WriteLine("Pastries: Buy One For $2 Or Three For $5.");
             // Console.WriteLine("Would you like to place a order? [Y/y for YES; Press Enter for No]");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             TryAgain();
 
 
           static void TryAgain()  
             {
 
-            Console.WriteLine("Would you like to place a order? [Y/y for YES; Press Enter for No]");
+            Console.WriteLine("Would You Like To Place a Order? [Y/y for YES; Press Enter For No]");
             string userInputOrder = Console.ReadLine().ToUpper();
             if (userInputOrder == "Y")
             {
                 int cost = 0;
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                Console.WriteLine("How many loafs of bread would you like?:");
+                Console.WriteLine("How Many Loafs Of Bread Would You Like?:");
                 int loafsWanted = int.Parse(Console.ReadLine());
-                Console.WriteLine("How many pastries would you like to purchase?:");
+                Console.WriteLine("How Many Pastries Would You Like To Purchase?:");
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 int pastriesWanted = int.Parse(Console.ReadLine());
 
@@ -49,21 +49,21 @@ namespace PierresBakery.Program
                         cost -= (item.Counter/3);
                     }
                     Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                    Console.WriteLine("Your order is: ");
+                    Console.WriteLine("Your Order Is: ");
                     Console.WriteLine($" Bread: {loafsWanted}, Pastries {pastriesWanted}");
-                    Console.WriteLine($" Your Order Total is: {cost}");
-                    Console.WriteLine("Please enter your Payment: ");
+                    Console.WriteLine($" Your Order Total Is: {cost}");
+                    Console.WriteLine("Please Enter Your Payment: ");
                     Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
                     int paymentResponse = int.Parse(Console.ReadLine());
                     
                     if(paymentResponse == cost)
                     {
-                        Console.WriteLine("Thank you for shopping with us!");
+                        Console.WriteLine("Thank You For Shopping With Us!");
                     }
                     else 
                     {
-                        Console.Write("My apologies, but that response was not valid please try again.");
+                        Console.Write("My Apologies, But That Response Was Not Valid Please Try Again.");
                         TryAgain();
                     }
 
---
+++ README.md
@@ -0,0 +1,34 @@
+# _{Pierre's Bakery}_
+
+#### By _Brandon James Fenk_
+
+#### _ C# Independent---Pierre's Bakery_
+
+## Technologies Used
+
+* _C#_
+* _.NET6_
+* _MS Testing_
+
+
+## Description
+
+_This is a console appication for Pierre's Bakery to calculate cost of bread and pastries purchased. It uses custom classes and auto implemented properties to create orders and output a total cost._
+
+## Setup/Installation Requirements
+
+* _Clone down the GitHub repo from here https://github.com/BrandonF55/Pierres-Bakery_
+* _Restore is required for packages: Change _
+* _While in Pierres-Bakery-Solutions root directory run $ dotnet build to build the program_
+* _While in Pierres-Bakery-Solutions root directory run $ dotnet run to run the program _
+
+
+## Known Bugs
+
+* _No Bugs at this current time_
+* _If you run into any bugs while running this console appication please feel free to email me at: brandon.fenk99@gmail.com_
+
+## License
+
+
+Copyright (c) _2022_ _Brandon James Fenk MIT_
