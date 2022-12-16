2fae75f2faa71e8bdcf20ff604602fa1fcae17d0
BrandonF55 <brandon.fenk99@gmail.com>
2022-12-16 09:58:07 -0800

    Have a 3rd passing test for orders

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -28,13 +28,23 @@ public class OrderTests : IDisposable
     string Item = "Bread";
     string Amount = "69 loafs";
     string Price = "$55";
-    string Date = "october";
+    string Date = "october 30th 2022";
     Order newOrder = new Order(Item, Amount, Price, Date);
-    string ItemCheck = newOrder.Item;
-    Assert.AreEqual(ItemCheck, Item);
+    string itemCheck = newOrder.Item;
+    Assert.AreEqual(itemCheck, Item);
   }
 
-  
+  [TestMethod]
+  public void OrderAmount_ReturnsOrderAmount_string()
+  {
+    string Item = "Bread";
+    string Amount = "69 loafs";
+    string Price = "$55";
+    string Date = "october 30th 2022";
+    Order newOrder = new Order(Item, Amount, Price, Date);
+    string amountCheck = newOrder.Amount;
+    Assert.AreEqual(amountCheck, Amount);
+  }
 
 
 
