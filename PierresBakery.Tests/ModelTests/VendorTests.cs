7345beb4aa0780377bc502b053d7f53745b8d920
BrandonF55 <brandon.fenk99@gmail.com>
2022-12-16 13:05:51 -0800

    a passing its 1st passing test for vendors

--- PierresBakery.Tests/ModelTests/VendorTests.cs
+++ PierresBakery.Tests/ModelTests/VendorTests.cs
@@ -6,4 +6,31 @@ using System;
 namespace PierresBakery.Test
 {
 
+
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreateInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("testing", "testing");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+
+      // string name = " Brandon's Bakery";
+      // string location = "first in sandy"
+      // Vendor newVendor = new Vendor(name, location)
+
+  }
 }
+
+
+
+
